13b417768482ade117133e163d286a8af815997b
AhmedK001 <mp8ytt@gmail.com>
2024-09-22 18:32:47 +0300

    Now system check if user's data are unique using EF Core, Implmented UserRepository core methods || Implmented EF Core querys methods that able to find user data using ID, Email or PhoneNumber with join operations between tables when needed

---
+++ Application/Mappers/StringToIntMapper.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Mappers;
+
+public class stringToIntMapper
+{
+    
+}
---
+++ Application/Services/SearchDataService/SearchUserService.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Services.SearchDataService;
+
+public class SearchUserService
+{
+    
+}
